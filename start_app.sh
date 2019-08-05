71544424550179ee03d3ac2314a25bbcec2727d5
brandonbryant12 <brandonbryant002@gmail.com>
2019-08-05 15:43:20 -0400

    adding start script

---
+++ start_app.sh
@@ -0,0 +1,9 @@
+#sudo docker pull anypay/SportsOracle
+
+docker build --tag sports_oracle .
+
+docker kill sports_oracle
+docker rm sports_oracle
+
+docker run -d --restart=always -p 3000:3000 --name sports_oracle --env-file=.env sports_oracle
+
