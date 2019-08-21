e76b1389ac0dfd22a5e06e2e637e0127348c255d
brandonbryant12 <brandonbryant002@gmail.com>
2019-08-21 14:35:09 -0400

    uncomment for each loop

--- sports-app/app/routes/home.js
+++ sports-app/app/routes/home.js
@@ -12,18 +12,21 @@ export default Ember.Route.extend({
 
      $.getJSON('/api/oracle-key/1').then((resp)=>{
 	     
+       console.log('oracleKey', resp);
        controller.set('oracleKey', resp);
 	     
      });
     
      $.getJSON('/api/games/today').then((resp)=>{
 	     
+       console.log('games', resp);
        controller.set('games', resp);
 	     
      });
    
      $.getJSON('/api/proposals').then((resp)=>{
 	     
+       console.log('proposals', resp);
        controller.set('proposals', resp);
 	     
      });
